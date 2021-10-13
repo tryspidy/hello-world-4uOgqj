0bdd08efe0a362ab4dc38c0d5ba8bbae9823aab5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-13 22:46:51 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,9 @@
+// I can't believe there is no grepper answer here!
+
+// Here is Java hello world:
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
